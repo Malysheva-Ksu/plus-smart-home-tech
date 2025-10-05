4b20ac3111cb194ccd5955b0fbb38361042659c8
Kseniia <kore-ksenia@yandex.ru>
2025-10-06 00:48:21 +0300

    переименование eventType

--- telemetry/collector/src/main/java/dto/abstractDto/HubEventDto.java
+++ telemetry/collector/src/main/java/dto/abstractDto/HubEventDto.java
@@ -18,7 +18,7 @@ import java.time.Instant;
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
         include = JsonTypeInfo.As.EXISTING_PROPERTY,
-        property = "eventType",
+        property = "type",
         defaultImpl = UnknownHubEventDto.class
 )
 @JsonSubTypes({
--- telemetry/collector/src/main/java/dto/hub/DeviceAddedEventDto.java
+++ telemetry/collector/src/main/java/dto/hub/DeviceAddedEventDto.java
@@ -1,18 +1,20 @@
 package dto.hub;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dto.abstractDto.HubEventDto;
 import dto.enums.DeviceTypeDto;
 import dto.enums.HubEventType;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class DeviceAddedEventDto extends HubEventDto {
     @NotBlank
     private String id;
+
     @NotNull
     private DeviceTypeDto type;
 
