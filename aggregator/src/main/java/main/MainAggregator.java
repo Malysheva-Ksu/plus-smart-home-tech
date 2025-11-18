2ba96f4131d68384e8bd58741db79e1bf97c4472
Kseniia <kore-ksenia@yandex.ru>
2025-11-18 23:00:53 +0300

    работа по теории

--- aggregator/src/main/java/main/MainAggregator.java
+++ aggregator/src/main/java/main/MainAggregator.java
@@ -2,11 +2,13 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import service.AggregationStarter;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @ComponentScan(basePackages = {"service", "kafka", "main"})
 public class MainAggregator {
 
