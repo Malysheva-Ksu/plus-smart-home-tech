a87fc2e9339165f4f114564461fe2dd4bcdd558e
Kseniia <kore-ksenia@yandex.ru>
2025-12-07 22:49:12 +0300

    добавила скрипт

---
+++ init-db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE warehouse;
+\c warehouse
+
+CREATE DATABASE shopping-store;
+\c shopping-store
+
+CREATE DATABASE shopping-cart;
+\c shopping-cart
+
+CREATE DATABASE analyzer;
+\c analyzer
