d1b564452a0a8384a1d2334db11e5e2add2b08ba
Kseniia <kore-ksenia@yandex.ru>
2025-12-08 22:17:12 +0300

    добавлена аннотация

--- commerce/interaction/src/main/java/model/shoppingStore/Product.java
+++ commerce/interaction/src/main/java/model/shoppingStore/Product.java
@@ -3,8 +3,10 @@ package model.shoppingStore;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
+@Entity
 @Data
 public class Product {
     private Long id;
